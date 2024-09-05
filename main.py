c9f68ab4b1005c645d85c9ed58013d5700df872b
Abhinavnair13 <92390306+Abhinavnair13@users.noreply.github.com>
2024-09-05 06:02:27 +0000

    Open close principle

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from abc import ABC, abstractmethod
+
+
+
 class Order:
   def __init__(self,items = [],quantity = [],prices =[],status = "open") -> None:
     self.items = items
@@ -15,13 +19,22 @@ class Order:
       
     return sum
 class Payment:
-  def pay(self, order:Order, paymentType : str, code):
-    order.status = 'paid'
-    print(f'Payment method: {paymentType}  code : {code}')
-    print(f'Order total: {order.get_total()}')
+  @abstractmethod
+  def pay(self, order:Order, paymentType : str, code,status: str):
+    ...
+
+class Paypal(Payment):
+  def pay(self, order:Order, paymentType : str, code,status: str):
+    print("Paying with Paypal")
+    print(f"Order: {order.items}")
+    print(f"Total: {order.get_total()}")
+    print(f"Payment Type: {True}")
+    print(f"Code: {code}")
+    print(f"Status: {status}")
 order = Order()
 order.add_item(item = 'iPhone',quantity = 1,price = 1000)
-pay = Payment()
-pay.pay(order = order,paymentType = 'card',code = '1234')
+paypal = Paypal()
+paypal.pay(order = order,paymentType ='card',code = '1234',status = 'paid')
 print(order.status)
+
     
